06b6729374c2165258566fc126f6fbbe7e44ba41
Elliot Trejo <elliot.trejo.tcu@gmail.com>
2019-11-19 19:22:58 -0600

    Added clock HTML and JS from previous activities and modified it to start at 75 seconds.

--- jscript.js
+++ jscript.js
@@ -70,6 +70,45 @@ function startQuiz() {
   
 }
 
+function startTimer() {
+
+
+  const clockEl = document.getElementById("clock");
+  let savedValue = localStorage.getItem("time");
+  let time = 75;
+  
+  let myInterval = null;
+
+    if (myInterval) {
+      clearInterval(myInterval);
+      myInterval = null;
+    }
+    myInterval = setInterval(function() {
+      time = time - 1;
+
+      let hours = Math.floor(time / 60 / 60);
+      let minutes = Math.floor(time / 60) % 60;
+      let seconds = time % 60;
+
+      if (hours < 10) {
+        hours = "0" + hours;
+      }
+      if (minutes < 10) {
+        minutes = "0" + minutes;
+      }
+      if (seconds < 10) {
+        seconds = "0" + seconds;
+      }
+
+      clockEl.innerHTML = hours + ":" + minutes + ":" + seconds;
+
+      if (time <= 0) {
+        clearInterval(myInterval);
+      }
+    }, 1000);
+  }
+  startTimer();
+
 
 //Received help from William on these functions
 
@@ -93,110 +132,15 @@ function displayQuestion(x) {
 }
 
 
+
+
 function checkAnswer() {
     displayQuestion(counter);
     counter++;
 }
 
-// // create some variables
+// function keepScore(){
 
-// const lastQuestion = questions.length - 1;
-// let runningQuestion = 0;
-// let count = 0;
-// const questionTime = 10; // 10s
-// const gaugeWidth = 150; // 150px
-// const gaugeUnit = gaugeWidth / questionTime;
-// let TIMER;
-// let score = 0;
+//    const  
 
-// // render a question
-// function renderQuestion(){
-//     let q = questions[runningQuestion];
-
-//     question.innerHTML = "<p>"+ q.question +"</p>";
-//     qImg.innerHTML = "<img src="+ q.imgSrc +">";
-//     choiceA.innerHTML = q.choiceA;
-//     choiceB.innerHTML = q.choiceB;
-//     choiceC.innerHTML = q.choiceC;
-// }
-
-// // render progress
-// function renderProgress(){
-//     for(let qIndex = 0; qIndex <= lastQuestion; qIndex++){
-//         progress.innerHTML += "<div class='prog' id="+ qIndex +"></div>";
-//     }
-// }
-
-// // counter render
-
-// function renderCounter(){
-//     if(count <= questionTime){
-//         counter.innerHTML = count;
-//         timeGauge.style.width = count * gaugeUnit + "px";
-//         count++
-//     }else{
-//         count = 0;
-//         // change progress color to red
-//         answerIsWrong();
-//         if(runningQuestion < lastQuestion){
-//             runningQuestion++;
-//             renderQuestion();
-//         }else{
-//             // end the quiz and show the score
-//             clearInterval(TIMER);
-//             scoreRender();
-//         }
-//     }
-// }
-
-// // checkAnwer
-
-// function checkAnswer(answer){
-//     if( answer == questions[runningQuestion].correct){
-//         // answer is correct
-//         score++;
-//         // change progress color to green
-//         answerIsCorrect();
-//     }else{
-//         // answer is wrong
-//         // change progress color to red
-//         answerIsWrong();
-//     }
-//     count = 0;
-//     if(runningQuestion < lastQuestion){
-//         runningQuestion++;
-//         renderQuestion();
-//     }else{
-//         // end the quiz and show the score
-//         clearInterval(TIMER);
-//         scoreRender();
-//     }
-// }
-
-// // answer is correct
-// function answerIsCorrect(){
-//     document.getElementById(runningQuestion).style.backgroundColor = "#0f0";
-// }
-
-// // answer is Wrong
-// function answerIsWrong(){
-//     document.getElementById(runningQuestion).style.backgroundColor = "#f00";
-// }
-
-// // score render
-// function scoreRender(){
-//     scoreDiv.style.display = "block";
-
-//     // calculate the amount of question percent answered by the user
-//     const scorePerCent = Math.round(100 * score/questions.length);
-
-//     // choose the image based on the scorePerCent
-//     let img = (scorePerCent >= 80) ? "img/5.png" :
-//               (scorePerCent >= 60) ? "img/4.png" :
-//               (scorePerCent >= 40) ? "img/3.png" :
-//               (scorePerCent >= 20) ? "img/2.png" :
-//               "img/1.png";
-
-//     scoreDiv.innerHTML = "<img src="+ img +">";
-//     scoreDiv.innerHTML += "<p>"+ scorePerCent +"%</p>";
 // }
